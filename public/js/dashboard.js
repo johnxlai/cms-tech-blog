4b58af98ffad5a2070d8847cad64b6b25717c0e6
John Lai <lai.john.py@gmail.com>
2023-03-01 18:02:38 -0500

    Add Dashboard client side js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,24 @@
 const newPost = document.querySelector('.js-new-post-btn');
 
-newPost.addEventListener('click', function () {
-  console.log('hello');
-});
+const newPostHandler = async (e) => {
+  e.preventDefault();
+  const postTitle = document.querySelector('#post-title').value.trim();
+  const postContent = document.querySelector('#post-constent').value.trim();
+
+  if (postTitle && postContent) {
+    const response = await fetch(`/api/blogs`, {
+      method: 'POST',
+      body: JSON.stringify({ postTitle, postContent }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create blog post');
+    }
+  }
+};
+newPost.addEventListener('click', newPostHandler);
