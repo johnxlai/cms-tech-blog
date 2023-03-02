17d734c7e6edba996b32f7067d00616551756034
John Lai <lai.john.py@gmail.com>
2023-03-01 21:40:42 -0500

    Add description to blog

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,13 @@
 const newPost = document.querySelector('.js-new-post-btn');
+//form
+const createPostForm = document.querySelector('.create-post-form');
 
 const newPostHandler = async (e) => {
   e.preventDefault();
   const postTitle = document.querySelector('#post-title').value.trim();
-  const postContent = document.querySelector('#post-constent').value.trim();
+  const postContent = document.querySelector('#post-content').value;
+
+  console.log('test');
 
   if (postTitle && postContent) {
     const response = await fetch(`/api/blogs`, {
@@ -21,4 +25,4 @@ const newPostHandler = async (e) => {
     }
   }
 };
-newPost.addEventListener('click', newPostHandler);
+createPostForm.addEventListener('submit', newPostHandler);
