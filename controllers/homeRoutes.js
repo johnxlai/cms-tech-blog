14e8ee16a6131439589ec5075f784a16406802ca
John Lai <lai.john.py@gmail.com>
2023-03-01 21:13:48 -0500

    Add user blog post on dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -39,7 +39,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
     });
 
     const user = userData.get({ plain: true });
-
+    console.log(user);
     res.render('dashboard', { ...user, loggedIn: true });
   } catch (err) {
     res.status(500).json(err);
