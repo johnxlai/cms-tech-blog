9fdf7ea69d3c7a573b905075799431ea2f87f867
John Lai <lai.john.py@gmail.com>
2023-03-07 22:19:34 -0500

    Fix blog delete btn

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -31,7 +31,7 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/blogs/${id}`, {
+    const response = await fetch(`/api/blog/${id}`, {
       method: 'DELETE',
     });
 
