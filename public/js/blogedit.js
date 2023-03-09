e7bbe99cabca4785fa8d8fd439b04ea32282d3d4
John Lai <lai.john.py@gmail.com>
2023-03-08 23:49:15 -0500

    Clean js to edit post

--- public/js/blogedit.js
+++ public/js/blogedit.js
@@ -6,39 +6,6 @@ const editBtn = document.querySelector('.edit-btn');
 
 const date = new Date();
 
-//Show new blog form
-// function showForm() {
-//   editPostForm.classList.remove('hidden');
-// }
-
-//hide new blog form
-// function hideForm() {
-//   editPostForm.classList.add('hidden');
-// }
-
-//Add blog
-// const newPostHandler = async (e) => {
-//   e.preventDefault();
-//   const title = document.querySelector('#post-title').value.trim();
-//   const post_content = document.querySelector('#post-content').value;
-
-//   if (title && post_content) {
-//     const response = await fetch(`/api/blog`, {
-//       method: 'POST',
-//       body: JSON.stringify({ title, post_content }),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/dashboard');
-//     } else {
-//       alert('Failed to create blog post');
-//     }
-//   }
-// };
-
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
@@ -73,17 +40,10 @@ const updateFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      alert('Failed to create blog post');
+      alert('Failed to update blog post');
     }
   }
 };
-
-//Show form for creation
-// newPostBtn.addEventListener('click', showForm);
-
-//Create new post
-// editPostForm.addEventListener('submit', editPostHandler);
-
 //Delete existing post
 deleteFormBtn.addEventListener('click', delButtonHandler);
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -55,27 +55,27 @@ const newPostHandler = async (e) => {
 //   }
 // };
 
-const updateFormHandler = async (event) => {
-  e.preventDefault();
-  const title = document.querySelector('#update-post-title').value.trim();
-  const post_content = document.querySelector('#update-post-content').value;
+// const updateFormHandler = async (event) => {
+//   e.preventDefault();
+//   const title = document.querySelector('#update-post-title').value.trim();
+//   const post_content = document.querySelector('#update-post-content').value;
 
-  if (title && post_content) {
-    const response = await fetch(`/api/blog`, {
-      method: 'PUT',
-      body: JSON.stringify({ title, post_content }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+//   if (title && post_content) {
+//     const response = await fetch(`/api/blog`, {
+//       method: 'PUT',
+//       body: JSON.stringify({ title, post_content }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to create blog post');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/dashboard');
+//     } else {
+//       alert('Failed to create blog post');
+//     }
+//   }
+// };
 
 //Show form for creation
 newPostBtn.addEventListener('click', showForm);
