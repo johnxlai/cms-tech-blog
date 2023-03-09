9a2d27cbdc96c126fb57e6820f6949696175af40
John Lai <lai.john.py@gmail.com>
2023-03-09 16:54:48 -0500

    Add test comment divs

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//New comment
+router.post('/comment/:id', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      body: req.body.body,
+      blog_id: req.params.id,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
